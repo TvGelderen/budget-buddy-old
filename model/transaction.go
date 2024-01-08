3caea1a53d70e16e4c74125caef7c4e0d9032922
TvGelderen <tvgelderen@outlook.com>
2024-01-08 20:41:26 +0100

    updated tables

--- model/transaction.go
+++ model/transaction.go
@@ -1,7 +1,12 @@
 package model
 
+import "time"
+
 type Transaction struct {
     Amount float64
     Incoming bool
     Recurring string
+    Date time.Time
+    NextDate time.Time
+    EndDate time.Time
 }
--- sql/schema/001_init.sql
+++ sql/schema/001_init.sql
@@ -10,12 +10,25 @@ CREATE TABLE users (
 
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL,
     amount DOUBLE PRECISION NOT NULL,
     incoming BOOLEAN NOT NULL,
-    recurring TEXT NOT NULL
+    recurring TEXT NOT NULL,
+    date TIMESTAMP NOT NULL,
+    next_date TIMESTAMP,
+    end_date TIMESTAMP,
+    CONSTRAINT fk_user_id
+        FOREIGN KEY(user_id)
+        REFERENCES users(id) 
+        ON DELETE CASCADE
 );
+
+CREATE INDEX id_idx ON users(id);
+CREATE INDEX user_id ON transactions(user_id);
 
 -- +goose Down
 DROP TABLE transactions;
 DROP TABLE users;
+
+DROP INDEX id_idx ON users(id);
+DROP INDEX user_id ON transactions(user_id);
